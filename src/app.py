771d7aa349634dbdc539c42b3d53e5f76365843e
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2024-03-31 15:33:09 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,11 +5,3 @@ from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
-
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
-
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
-
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
-
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
